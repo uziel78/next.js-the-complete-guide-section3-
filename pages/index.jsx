0ae1d2e975224e37509db188523df4fd18aba85e
uziel78 <rune.husa78@gmail.com>
2021-10-25 15:22:47 +0200

    init Section 4 project

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,7 @@
 import Link from "next/link";
 
 function HomePage() {
-  return (
-    <div>
-      <h1>The Home Page</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio Page</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients Page</Link>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default HomePage;
