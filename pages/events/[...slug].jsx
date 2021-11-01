65838e314d831aa7174e7c1d258c79c98f906ccf
uziel78 <rune.husa78@gmail.com>
2021-11-01 21:03:17 +0100

    Section 6 Init

---
+++ pages/events/[...slug].jsx
@@ -0,0 +1,82 @@
+import { useRouter } from "next/router";
+import Button from "../../components/ui/button";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
+import ResultsTitle from "../../components/events/ResultsTitle";
+import ErrorAlert from "../../components/ui/ErrorAlert";
+
+function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+  console.log(filterData);
+
+  // if filterData is undefined (as it is by default when useRouter() is first run)
+  // add a loading indicator
+  if (!filterData) {
+    return (
+      <>
+        <p>Loading...</p>
+        <div className="center"></div>
+      </>
+    );
+  }
+
+  // access url array data, which are strings by default in the url
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+  // adding a "+" transforms the string to a number...
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  // validity check
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2050 ||
+    numYear < 2020 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <ErrorAlert>
+        <p className="center">Invalid filter. Please adjust your values.</p>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </ErrorAlert>
+    );
+  }
+
+  // call getFilteredEvents function and pass in object
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  // check if filteredEvent matches with any current data
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No event found for current search</p>
+          <div className="center">
+            <Button link="/events">Show All Events</Button>
+          </div>
+        </ErrorAlert>
+      </>
+    );
+  }
+
+  // create date variable to pass into ResultsTitle component below
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
+  );
+}
+
+export default FilteredEventsPage;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,60 +1,14 @@
-import path from "path";
-// node.js module
-import fs from "fs/promises";
-//imports file system module from node.js (core node.js module).
-import Link from "next/link";
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/EventList";
 
-function HomePage(props) {
-  const { products } = props;
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
 
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>{product.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
   );
-}
-
-// getStaticProps is a page function that executed on the server-side and
-// it and any code within is never visible to the client (although props are passed along).
-// on server-side, on cane work with the file system (se fs import above).
-
-export async function getStaticProps(context) {
-  //construct filePath using node.js objects, finding current working directory
-  // and establishing a path from here. node.js will treat current files as if in root dir by default (not in pages folder),
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
-
-  // shows update in development server
-  console.log("(RE-)Generating...");
-
-  // using the redirect key to take one to new page if no data
-  if (!data) {
-    return {
-      redirect: {
-        destination: "/redirect-path",
-      },
-    };
-  }
-
-  // example if no object returned from props, automatically takes one to the 404 page.
-  if (data.products.length === 0) {
-    return { notFound: true };
-  }
-
-  // return object
-  return {
-    props: {
-      products: data.products,
-    },
-    // second key used to update content on page every X seconds (optional)
-    // note: development server always runs with latest data
-    revalidate: 60,
-  };
 }
 
 export default HomePage;
